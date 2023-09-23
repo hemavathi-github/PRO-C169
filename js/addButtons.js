6bf2b59d8740e151d501acaf9819d1d3b44157b9
hemavathi-github <75414397+hemavathi-github@users.noreply.github.com>
2023-09-23 12:57:17 +0530

    Update addButtons.js

--- js/addButtons.js
+++ js/addButtons.js
@@ -6,11 +6,8 @@ AFRAME.registerComponent("create-buttons", {
     button1.setAttribute("id", "rating-button");
     button1.setAttribute("class", "btn btn-warning mr-3");
 
-    // 2. Create the button
-    var button2 = document.createElement("button");
-    button2.innerHTML = "ORDER NOW";
-    button2.setAttribute("id", "order-button");
-    button2.setAttribute("class", "btn btn-warning mr-4");
+    // 2. Create the button for order
+   
 
     // 2. Append somewhere
     var buttonDiv = document.getElementById("button-div");
